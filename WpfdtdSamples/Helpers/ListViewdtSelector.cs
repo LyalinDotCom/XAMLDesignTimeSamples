4278397657e56034176c7e61d19ef860b6a9f9df
Dmitry Lyalin <dmitry.lyalin@lyalin.com>
2020-06-24 22:13:17 -0700

    Added ListView with TemplateSelector

---
+++ WpfdtdSamples/Helpers/ListViewdtSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfdtdSamples.Helpers
+{
+    public class ListViewdtSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement elemnt = container as FrameworkElement;
+
+            var value = item as string;
+            if (value == "Donovan")
+            {
+                return elemnt.FindResource("ListViewBoldDataTemplate") as DataTemplate;
+            } else
+            {
+                return elemnt.FindResource("ListViewDefaultDataTemplate") as DataTemplate;
+            }
+
+            //FrameworkElement elemnt = container as FrameworkElement;
+            //User user = item as User;
+            //if (user.IsPremiumUser)
+            //{
+            //    return elemnt.FindResource("PremiumUserDataTemplate") as DataTemplate;
+            //}
+            //else
+            //{
+            //    return elemnt.FindResource("NormalUserDataTemplate") as DataTemplate;
+            //}
+        }
+    }
+}

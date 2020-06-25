176cd371ea03b8aac2a03be42000752672dbf5b2
Dmitry Lyalin <dmitry.lyalin@lyalin.com>
2020-06-25 09:21:14 -0700

    More controls and first one using a model entity 'Employee' instead of just string array

---
+++ WpfdtdSamples/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfdtdSamples.Models
+{
+    public enum OrderStatus { None, New, Processing, Shipped, Received };
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int IsMember { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
